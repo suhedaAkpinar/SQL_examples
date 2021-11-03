b0024c57d2aafe7b73953481efbfca8ec2bbc427
Suheda <75789656+suhedaAkpinar@users.noreply.github.com>
2021-11-03 13:59:52 +0300

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -9,8 +9,7 @@ Kolay Gelsin.
 
 */
 
-
-SELECT * FROM film WHERE  replacement_cost>=12.99 AND replacement_cost<16.99 ;
+SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 and 16.99;
 
 
 SELECT first_name, last_name from actor WHERE first_name IN('Penelope','Nick','Ed');
